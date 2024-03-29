314d508297bcb1f98b26df121532c1748af15f07
Douile <douile@douile.com>
2024-03-29 14:12:34 +0000

    install rustup

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,33 @@
 import * as core from '@actions/core';
 import { exec } from '@actions/exec';
-
+import * as tc from '@actions/tool-cache';
 
 const image = 'ghcr.io/cross-rs/aarch64-unknown-linux-gnu:latest';
 
+/**
+ * Wrap an async function in a actions group.
+ * @param name Name of the actions group
+ * @param func Async function
+ * @returns Void promise.
+ */
+function groupWrap(name: string, func: () => Promise<void>): () => Promise<void> {
+  return async function() {
+    core.startGroup(name);
+    try {
+      await func();
+    } finally {
+      core.endGroup();
+    }
+  }
+}
+
+const installRustup = groupWrap("install toolchain", async () => {
+  await tc.downloadTool("https://sh.rustup.rs", "rustup.sh");
+  exec('bash', ['rustup.sh']);
+});
+
 async function run() {
+  await installRustup();
   await exec('docker', ['pull', image]);
   await exec('docker', ['run', '--rm', image]);
 }
